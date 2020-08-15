6379c0f56afb7144f2ac975d7496ae67c6864bea
Sinésio Neto <sn.sineto@gmail.com>
2020-08-15 02:13:08 -0300

    chore: configure express

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('bodyParser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log('server started at http://localhost:3001'));
